7bc3fa495b3b8548551940baa6f8eebd4bfc661b
Leon Dore <leon.dore@gmail.com>
2024-10-27 09:24:16 -0400

    established connection to rabbitmq server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const (
+	CONN_URL = "amqp://guest:guest@localhost:5672/"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	conn, err := amqp.Dial(CONN_URL)
+	if err != nil {
+		log.Fatalf("could not connect to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+
+	fmt.Println("connection to RabbitMQ server was successful")
+
+	// Wait for user termination signal
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	<-shutdown
+	fmt.Println("closing connection to RabbitMQ server")
 }
